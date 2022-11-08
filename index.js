3c7f952d2c508ce2a986e62df5b5816db7f34ca1
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2022-11-08 03:52:07 +0200

    Fix bugs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import { createRoot } from 'react-dom/client';
+
 import { Header } from './src/header';
 import { Main } from './src/main';
 import { Footer } from './src/footer';
-import { createRoot } from 'react-dom/client';
 
 const root = createRoot( document.getElementById('app') );
 const component = (
